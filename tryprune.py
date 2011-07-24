36b055a68d3dbb9c16a85dec9aa5449b042d2090
Peng Wu <alexepico@gmail.com>
2011-07-24 18:55:45 +0800

    begin to write tryprune.py

---
+++ tryprune.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from subprocess import Popen, PIPE
+from argparse import ArgumentParser
+
+def validateModel(modelfile):
+    pass
+
+def exportModel(modelfile, textmodel):
+    pass
+
+def mergeOneModel(mergedmodel, onemodel):
+    pass
+
+def mergeSomeModels(indexfile, mergenum):
+    pass
+
+def pruneModel(modelfile, k, CDF):
+    pass
+
+if __name__ == '__main__':
+    pass
