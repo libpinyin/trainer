8a09db5ce595569550475c03aeb9f025b942d128
Peng Wu <alexepico@gmail.com>
2013-01-18 15:36:30 +0800

    write load_words function

--- partialword.py
+++ partialword.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import os
-import os.path
 import sqlite3
+from argparse import ArgumentParser
+import utils
+from myconfig import MyConfig
 
 
 SELECT_PARTIAL_WORD_DML = '''
@@ -40,3 +42,36 @@ DROP TABLE IF EXISTS ngram_fts;
 SELECT_MERGE_HIGH_NGRAM_DML = '''
 SELECT words, freq FROM ngram_fts WHERE words MATCH ?;
 '''
+
+config = MyConfig()
+
+#maximum combine number
+N = config.getMaximumCombineNumber()
+
+#change cwd to the word recognizer directory
+words_dir = config.getWordRecognizerDir()
+os.chdir(words_dir)
+#chdir done
+
+
+#load existing words
+words_set = set([])
+
+def load_words(filename):
+    wordlistfile = open(filename, "r")
+
+    for oneline in wordlistfile.readlines():
+        oneline = oneline.rstrip(os.linesep)
+
+        if len(oneline) == 0:
+            continue
+
+        word = oneline
+
+        words_set.add(word)
+
+    wordlistfile.close()
+
+
+load_words(config.getWordsListFileName())
+#print(words_set)
